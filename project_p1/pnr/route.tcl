b04b1ac542f8da9a58036769d58c7981f0104f55
tonywu3219 <tonywu3219@gmail.com>
2025-03-08 18:56:44 -0800

    Update route.tcl

--- project_p1/pnr/route.tcl
+++ project_p1/pnr/route.tcl
@@ -21,5 +21,7 @@ optDesign -postRoute -setup -hold
 # Fix DRC errors
 optDesign -postRoute -drv
 optDesign -postRoute -inc
+optDesign -postRoute -drv
+optDesign -postRoute -inc
 
 saveDesign route.enc
