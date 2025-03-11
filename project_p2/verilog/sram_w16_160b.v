43c8ae7ced7e0da0d2a6bc7cf64eb096459346df
tonywu3219 <tonywu3219@gmail.com>
2025-03-11 15:09:06 -0700

    Update and rename sram_160b_w16.v to sram_w16_160b.v

--- project_p2/verilog/sram_w16_160b.v
+++ project_p2/verilog/sram_w16_160b.v
@@ -1,33 +1,33 @@
 // Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
 // Please do not spread this code without permission 
-module sram_160b_w16 (CLK, D, Q, CEN, WEN, A);
+module sram_w16_160b (CLK, D, Q, CEN, WEN, A);
 
+  parameter sram_bit = 160;
   input  CLK;
   input  WEN;
   input  CEN;
-  input  [159:0] D;
+  input  [sram_bit-1:0] D;
   input  [3:0] A;
-  output [159:0] Q;
+  output reg [sram_bit-1:0] Q;
 
-  reg [159:0] memory0;
-  reg [159:0] memory1;
-  reg [159:0] memory2;
-  reg [159:0] memory3;
-  reg [159:0] memory4;
-  reg [159:0] memory5;
-  reg [159:0] memory6;
-  reg [159:0] memory7;
-  reg [159:0] memory8;
-  reg [159:0] memory9;
-  reg [159:0] memory10;
-  reg [159:0] memory11;
-  reg [159:0] memory12;
-  reg [159:0] memory13;
-  reg [159:0] memory14;
-  reg [159:0] memory15;
+  reg [sram_bit-1:0] memory0;
+  reg [sram_bit-1:0] memory1;
+  reg [sram_bit-1:0] memory2;
+  reg [sram_bit-1:0] memory3;
+  reg [sram_bit-1:0] memory4;
+  reg [sram_bit-1:0] memory5;
+  reg [sram_bit-1:0] memory6;
+  reg [sram_bit-1:0] memory7;
+  reg [sram_bit-1:0] memory8;
+  reg [sram_bit-1:0] memory9;
+  reg [sram_bit-1:0] memory10;
+  reg [sram_bit-1:0] memory11;
+  reg [sram_bit-1:0] memory12;
+  reg [sram_bit-1:0] memory13;
+  reg [sram_bit-1:0] memory14;
+  reg [sram_bit-1:0] memory15;
 
-  reg [3:0] add_q;
-
+/*
   assign Q = (add_q == 0)  ? memory0 : (
              (add_q == 1)  ? memory1 : (
              (add_q == 2)  ? memory2 : (
@@ -43,12 +43,32 @@ module sram_160b_w16 (CLK, D, Q, CEN, WEN, A);
              (add_q == 12) ? memory12 : (
              (add_q == 13) ? memory13 : (
              (add_q == 14) ? memory14 : memory15))))))))))))));
+*/
 
   always @ (posedge CLK) begin
 
-   if (!CEN && WEN) // read 
-      add_q <= A;
-   if (!CEN && !WEN) // write
+   if (!CEN && WEN) begin // read 
+     case (A)
+      4'b0000: Q <= memory0; 
+      4'b0001: Q <= memory1; 
+      4'b0010: Q <= memory2; 
+      4'b0011: Q <= memory3; 
+      4'b0100: Q <= memory4; 
+      4'b0101: Q <= memory5; 
+      4'b0110: Q <= memory6; 
+      4'b0111: Q <= memory7; 
+      4'b1000: Q <= memory8; 
+      4'b1001: Q <= memory9; 
+      4'b1010: Q <= memory10; 
+      4'b1011: Q <= memory11; 
+      4'b1100: Q <= memory12; 
+      4'b1101: Q <= memory13; 
+      4'b1110: Q <= memory14; 
+      4'b1111: Q <= memory15; 
+    endcase
+   end
+
+   else if (!CEN && !WEN) begin // write
      case (A)
       4'b0000: memory0  <= D; 
       4'b0001: memory1  <= D; 
@@ -68,5 +88,6 @@ module sram_160b_w16 (CLK, D, Q, CEN, WEN, A);
       4'b1111: memory15 <= D; 
     endcase
   end
+end
 
 endmodule
