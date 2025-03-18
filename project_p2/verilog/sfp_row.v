55d400a58c37792c77656d276d3ecde3eb41675e
tonywu3219 <tonywu3219@gmail.com>
2025-03-18 14:33:54 -0700

    Update sfp_row.v

--- project_p2/verilog/sfp_row.v
+++ project_p2/verilog/sfp_row.v
@@ -112,14 +112,14 @@ module sfp_row (clk, acc, div, fifo_ext_rd, sum_in, sum_out, sfp_in, sfp_out);
          fifo_wr <= 0;
    
          if (div) begin
-           sfp_out_sign0 <= {sfp_in_sign0, 8'b0} / sum_2core;
-           sfp_out_sign1 <= {sfp_in_sign1, 8'b0} / sum_2core;
-           sfp_out_sign2 <= {sfp_in_sign2, 8'b0} / sum_2core;
-           sfp_out_sign3 <= {sfp_in_sign3, 8'b0} / sum_2core;
-           sfp_out_sign4 <= {sfp_in_sign4, 8'b0} / sum_2core;
-           sfp_out_sign5 <= {sfp_in_sign5, 8'b0} / sum_2core;
-           sfp_out_sign6 <= {sfp_in_sign6, 8'b0} / sum_2core;
-           sfp_out_sign7 <= {sfp_in_sign7, 8'b0} / sum_2core;
+           sfp_out_sign0 <= {abs[bw_psum*1-1 : bw_psum*0], 8'b0} / sum_2core;
+           sfp_out_sign1 <= {abs[bw_psum*2-1 : bw_psum*1], 8'b0} / sum_2core;
+           sfp_out_sign2 <= {abs[bw_psum*3-1 : bw_psum*2], 8'b0} / sum_2core;
+           sfp_out_sign3 <= {abs[bw_psum*4-1 : bw_psum*3], 8'b0} / sum_2core;
+           sfp_out_sign4 <= {abs[bw_psum*5-1 : bw_psum*4], 8'b0} / sum_2core;
+           sfp_out_sign5 <= {abs[bw_psum*6-1 : bw_psum*5], 8'b0} / sum_2core;
+           sfp_out_sign6 <= {abs[bw_psum*7-1 : bw_psum*6], 8'b0} / sum_2core;
+           sfp_out_sign7 <= {abs[bw_psum*8-1 : bw_psum*7], 8'b0} / sum_2core;
 
 
 
